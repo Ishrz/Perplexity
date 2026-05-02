4b64cead9dfcc983c6c97d2e1a2df462a7f840f1
Ishrz <ishrzz66@gmail.com>
2026-05-02 23:23:46 +0530

    register controller :check user exist

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,6 +5,16 @@ export const register = async (req,res)=>{
 
     const {username, email , password} = req.body
 
+    const isUserExist = await userModel.findOne(
+        {$or:[{username} , {email}]}
+    )
+
+    if(isUserExist) return res.status(400).json({
+        message:"user already exist with this username or email try diffrent",
+        success:false,
+        error:"user already exist"
+    })
+
     const user =await userModel.create({
         username,
         email,
